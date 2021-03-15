d0870d1e0c2d1e60be786e9e1c47a5ac6bb2ecc4
Cristi <cristian.ciont@gmail.com>
2021-03-15 13:43:08 +0200

    basic tests

--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -1,11 +1,17 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestApp {
 
     @Test
     public void test1(){
         assertFalse(false);
+    }
+
+    @Test
+    public void test2(){
+        assertTrue(true);
     }
 }
