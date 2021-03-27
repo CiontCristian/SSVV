5d4d98595aea391f45d6aae9102157225f6309b5
Cristi <cristian.ciont@gmail.com>
2021-03-27 17:10:30 +0200

    6 junit test cases for addStudent

--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -29,6 +29,43 @@ public class TestApp {
     public void test2(){
         int result = testService.saveStudent("1","Cristi",110);
         assertEquals(0, result);
-
     }
+
+    @Test
+    public void test3(){
+        int result = testService.saveStudent("1","Cristi",938);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void test4(){
+        int result = testService.saveStudent(null,"Cristi",932);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void test5(){
+        int result = testService.saveStudent("","Cristi",932);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void test6(){
+        int result = testService.saveStudent("1",null,932);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void test7(){
+        int result = testService.saveStudent("1","",932);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void test8(){
+        int result = testService.saveStudent(null,"",109);
+        assertEquals(0, result);
+    }
+
+
 }
